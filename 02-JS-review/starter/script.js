56b05409ec59f07b71df77be7bfd3d991be71b6a
lei <littlesomerest@gmail.com>
2024-04-10 10:51:59 +0800

    immutable array: add, delete, update

--- 02-JS-review/starter/script.js
+++ 02-JS-review/starter/script.js
@@ -266,3 +266,30 @@ arr;
 
 const sortedByPages = books.slice().sort((a, b) => b.pages - a.pages);
 sortedByPages;
+
+// Immutable Array
+
+// 1. Add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Porter and the Chamber of Secrets",
+  author: "J.K.Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+// 2. Delete book object from array
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+// 3. Update book object in array
+const booksAfterUpdate = books.map((book) =>
+  book.id === 1
+    ? {
+        ...book,
+        pages: 9999,
+      }
+    : book
+);
+booksAfterUpdate;
